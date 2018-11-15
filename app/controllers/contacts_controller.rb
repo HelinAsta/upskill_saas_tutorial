587f444152b55c2ad7f03333ffd2602b839a3f16
Helin Kalef <helin.asta.kalef@live.no>
2018-11-15 16:00:03 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,42 @@
 class ContactsController < ApplicationController
-def new
-  @contact = Contact.new
-end
-
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-    name = params[:contact][:name]
-    email = params[:contact][:email]
-    body = params[:contact][:comments]
-    ContactMailer.contact_email(name, email, body).deliver
-    flash[:success] = "Message sent."
-    redirect_to new_contact_path
-  else
-    flash[:danger] = @contact.errors.full_messages.join(", ")
-     redirect_to new_contact_path
+  
+  #GET request to/contact-us
+  #Show new contact from
+  def new
+    @contact = Contact.new
   end
-end
-
-private
-  def contact_params
-     params.require(:contact).permit(:name, :email, :comments)
-  end  
-end
-
+  
+  #POST request /contacts
+  def create
+    #Mass assignment of form fields into contact object
+    @contact = Contact.new(contact_params)
+    #Save the contact object to the database
+    if @contact.save
+      #Store form fields via parameters into variables
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      #Plug variables into the contact Mailer
+      #email method and send email
+      ContactMailer.contact_email(name, email, body).deliver
+      #Store sucsess message in fralsh hash
+      #and redirect to the new action
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
+    else
+      #If Contact object doesn't save
+      #store errors in flash hash 
+      #and redirect to new action
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+       redirect_to new_contact_path
+    end
+  end
+  
+  private
+  #To collect data from form we need to use
+  #strong parameters and whitelist the form fields
+    def contact_params
+       params.require(:contact).permit(:name, :email, :comments)
+    end  
+  end
+  
