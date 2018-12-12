35e5bd33f5b3be9e817c7ab3f72b34dd4cd35b2b
Helin Kalef <helin.asta.kalef@live.no>
2018-12-12 18:28:55 +0000

    upodated flash message styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   
   attr_accessor :stripe_card_token
   #If Pro user passes validations (email, password, etc,), then call Strie
-  #and tell Stripe to set up a subscription upon charching the customer's cards
+  #and tell Stripes to set up a subscription upon charching the customer's cards
+  #Stripe responds data and we restore customer id as customer token and 
+  # Save to user
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
