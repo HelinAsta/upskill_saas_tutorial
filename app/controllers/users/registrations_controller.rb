df37708e85c4797af582d358c476beeb8cf069e2
Helin Kalef <helin.asta.kalef@live.no>
2018-12-13 15:19:24 +0000

    Corrected spelling mistake in flash message

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,7 +20,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
     def select_plan
       unless (params[:plan] == '1' || params[:plan] == '2')
-        flash[:notice] = "Pleace select a membership plan to sign up."
+        flash[:notice] = "Please select a membership plan to sign up."
         redirect_to root_url
       end
     end
